8b8f38a4c327207d180eea05c213fabae07bd1bd
ShekharDahima <ujjwalthakur929@gmail.com>
2025-02-17 09:58:38 +0000

    datatypes

---
+++ o1_Basics/02_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; // it means all code is treated as a newer version 
+
+// alert("hello")   // we are using nodejs not browser
+
+// read the doumentation on Ecma
+
+// primitive Data types 
+
+// number => 2 power 53
+//bigint   
+// string. =>""
+//boolean  =>true/false
+// undefined
+// null   => standalone value
+// symbol   // used to uniquily identifying components
+
+//object
+
+console.log(typeof null);       // object
+console.log(typeof undefined);      // undefined
+
